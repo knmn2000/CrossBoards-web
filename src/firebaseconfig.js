2d4bf6a80312db0a4b9a6f6caf1122c6ea7e6fd9
karan <karan@radicali.com>
2021-07-29 22:25:12 +0530

    update conf

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,5 +1,5 @@
 export const config = {
-  apiKey: 'API_KEY',
+  apiKey: 'AIzaSyBgpPqQ-4qZmihzKv2rFzl5vwqb7UuyiLY',
   projectId: 'crossboards',
   databaseURL: 'https://crossboards.firebaseio.com',
   authDomain: 'crossboards.firebaseapp.com',

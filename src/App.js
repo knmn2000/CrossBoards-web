e38470f4e6f1c37c4f9fa1846356d0d73ce5f3d6
karan <karan@radicali.com>
2021-07-29 17:59:31 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {
+  Grid,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  makeStyles,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flex: 1,
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    color: 'black',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  nav: {
+    backgroundColor: 'white',
+  },
+  authButton: {
+    fontSize: 30,
+    flexGrow: 1,
+    color: 'black',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    textAlignLast: 'end',
+  },
+}));
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Grid>
+        <AppBar position='static'>
+          <Toolbar className={classes.nav}>
+            <Grid container justifyContent='center'>
+              <Grid item xs />
+              <Grid item xs>
+                <Typography variant='h6' className={classes.title}>
+                  CrossBoards
+                </Typography>
+              </Grid>
+              <Grid item xs>
+                <IconButton className={classes.authButton}>
+                  <ExitToAppIcon />
+                </IconButton>
+              </Grid>
+            </Grid>
+            <Button color='inherit'>Login</Button>
+          </Toolbar>
+        </AppBar>
+        <Grid container style={{ flex: 1 }}>
+          <Grid item>
+            <Typography>Hello</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 }
